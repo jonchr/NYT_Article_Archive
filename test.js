407315276c72ba8c8def937640ef805d09e7f4b8
Jon <jonathan.lee.christensen@gmail.com>
2017-10-14 22:04:07 -0400

    removed test file

--- test.js
+++
@@ -1,8 +0,0 @@
-var axios = require("axios");
-
-
-function search () {
-	return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=88c9c75cbe3041429fd40ce162908059&q=apple");
-}
-
-console.log(search());
